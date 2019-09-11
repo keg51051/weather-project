aa28fd59dda4366bcc6bdacaaee4c0e9fb3eccbd
Hanseong Kim <keg51051@gmail.com>
2019-09-11 16:24:42 -0400

    commit

---
+++ Weather.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import { StyleSheet, Text, View} from 'react-native';
+import { LinearGradient } from "expo";
+
+export default class Weather extends Component {
+    render() {
+        return (
+            <LinearGradient
+                colors={["#00C6FB", "#005BEA"]}
+                style={styles.container}
+            />
+            );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    }
+
+});
